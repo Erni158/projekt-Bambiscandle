b0002b31758c7418d0c9fb0487d185eca5af4b26
Erni158 <ernest-kies@wp.pl>
2020-10-15 16:58:59 +0200

    Candles data

---
+++ src/data/candles.js
@@ -0,0 +1,52 @@
+const candles = [
+    {
+        id: 1,
+        name: "Autumn Fire"
+    },
+    {
+        id: 2,
+        name: "Halloween"
+    },
+    {
+        id: 3,
+        name: "Enchanted Forest"
+    },
+    {
+        id: 3,
+        name: "Witch's Poison"
+    },
+    {
+        id: 4,
+        name: "Morning Coffee"
+    },
+    {
+        id: 5,
+        name: "Cotton Cloud"
+    },
+    {
+        id: 6,
+        name: "Tranquility"
+    },
+    {
+        id: 7,
+        name: "Yule's Eve"
+    },
+    {
+        id: 8,
+        name: "Winter Solstice"
+    },
+    {
+        id: 9,
+        name: "Mermaid"
+    },
+    {
+        id: 10,
+        name: "Love Spell"
+    },
+    {
+        id: 11,
+        name: "Witch's Infusion"
+    }    
+]
+
+export default candles;
